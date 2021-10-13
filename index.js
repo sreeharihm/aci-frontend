7ba3b9a51c71fbdf88946ecbb8bf4ea3982d67ec
Sreehari Hareendran <Sreehari.Hareendran@dh.com>
2021-10-13 20:37:10 +0530

    corrected the connection

--- index.js
+++ index.js
@@ -2,44 +2,52 @@ const { DefaultAzureCredential } = require("@azure/identity");
 const { BlobServiceClient } = require("@azure/storage-blob");
 const XLSX = require('xlsx');
 const MongoClient = require('mongodb').MongoClient;
-const connectionString = "mongodb://exceltocosmos:8DIMWhlP1nTvBPhmW6OnbIAkaHUPVImIpGpp0nq2g319D1nzez4FJ4wvfqz8SiItI1tceGsrEx1lnI2PWFwJCw==@exceltocosmos.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@exceltocosmos@";
-
+const connectionString = "";
 const dbName = "testexcelimport";
 const account = "storage111021";
 const defaultAzureCredential = new DefaultAzureCredential();
 
 const blobServiceClient = new BlobServiceClient(
-  `https://${account}.blob.core.windows.net`
+  
 );
 
 const containerName = "testtrigger";
 
-var db
+var db;
 
 MongoClient.connect(connectionString, {
     useUnifiedTopology: true
   }, (err, client) => {
     if (err) return console.error(err)
-    context.log('Connected to Database')
-    db = client.db("testexcelimport")
-})
+    console.log('Connected to Database')
+    db = client.db(dbName)
+  })
 
 
 module.exports = async function (context, myBlob) {
     context.log("JavaScript blob trigger function processed blob \n Blob:", context.bindingData.blobTrigger, "\n Blob Size:", myBlob.length, "Bytes");
 
+    const fileType = context.bindingData.blobTrigger.split(".")[1];
+    if(fileType != "xlsx")
+    {
+      context.log("Invalid file type!!");
+      return;
+    }
+
     const workbook = XLSX.read(myBlob, {type:"buffer"});     
     const sheet_name_list = workbook.SheetNames;
+    const collectionName= '';
 
     for(var i=0;i < sheet_name_list.length; i++){
-      var data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[i]]) 
-      context.log(dbName);
-      context.log(sheet_name_list[i]);   
-      db.collection(sheet_name_list[i]).insertMany(data, (err, res) => {
-        if (err) throw err;   
-        context.log("Number of documents inserted: " + res.insertedCount);
-      });
+      var data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[i]]);  
+
+        context.log(sheet_name_list[i]);   
+        db.collection(sheet_name_list[i]).insertMany(data, (err, res) => {
+          if (err) throw err;   
+          console.log("Number of documents inserted: " + res.insertedCount);         
+        });
     }
+
     // Get blob content from position 0 to the end
     // In Node.js, get downloaded data by accessing downloadBlockBlobResponse.readableStreamBody
     // const downloadBlockBlobResponse = await blobClient.download();
